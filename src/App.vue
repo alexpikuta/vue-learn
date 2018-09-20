a09a78fe39454d8d72dcbc02abd78d012f0c1d28
alexpikuta <olexandr.pikuta@gmail.com>
2018-09-20 13:43:00 +0300

    Initial comit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <div class="container pt-5">
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          class="form-control"
+          :class="{'is-invalid': $v.email.$error}"
+          @blur="$v.email.$touch()"
+          v-model="email"
+        >
+        <div
+          v-show="$v.email.$error"
+          class="invalid-feedback">
+          Please provide a valid information
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  data () {
+    return {
+      email: ''
+    }
+  },
+  validations: {
+    email: {
+      required,
+      email
+    }
+  }
+}
+</script>
